e555e9ab3a9d87ea0538711fff1749bb3b9df0ba
pdp1208 <55811191+pdp1208@users.noreply.github.com>
2025-12-06 13:25:25 +0530

    Refactor containsDuplicate method with HashMap

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate1(int[] nums) {
         HashSet<Integer> setNum = new HashSet<Integer>();
         for (int i = 0; i < nums.length; i++) {
             setNum.add(nums[i]);
@@ -11,5 +11,17 @@ class Solution {
             return false;
         }
         return true;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+         HashMap<Integer, Integer> newMap = new HashMap<Integer, Integer>();
+         for (int i = 0; i < nums.length; i++) {
+            if(newMap.containsKey(nums[i])){
+                return true;
+            }else{
+                newMap.put(nums[i], 1);
+            }
+        }
+            return false;  
     }
 }
